eea8fffd076b124231aa30dd17ff34886267e137
Brian Ineza <brianineza01@gmail.com>
2025-01-27 07:36:27 +0100

    ch: initial setup

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["best-book-store-nextjs-ap-project_*"],
+} satisfies Config;
